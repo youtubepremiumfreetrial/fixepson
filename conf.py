f1cb963ce6fa8790fee6014c55cbb079402c7762
youtubepremiumfreetrial <165153215+youtubepremiumfreetrial@users.noreply.github.com>
2025-07-03 14:31:34 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'HP Smart'
+project = 'Fix your Epson printer'
 
 
 # The full version, including alpha/beta/rc tags
